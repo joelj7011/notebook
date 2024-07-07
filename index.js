e2827eb9cf6ed9412a392d94f6a4f79e98b6975c
Shivang Tiwari <93948068+joelj7011@users.noreply.github.com>
2024-07-07 15:47:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,29 +5,37 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000; // Use environment variable for port if available
+
+// Middleware to parse JSON and cookies
 app.use(express.json());
 app.use(cookieParser());
 
+// CORS configuration
 const corsOptions = {
     origin: ["http://localhost:3000", "https://notebok-frontend.onrender.com"],
-     credentials: true,
-    optionsSuccessStatus: 200
-}
-app.options('*', cors(corsOptions));
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+};
 
+// Enable CORS with the specified options
+app.use(cors(corsOptions));
+
+// Log each request to help debug CORS issues
 app.use((req, res, next) => {
     console.log(`Received ${req.method} request for ${req.url}`);
     console.log('Request headers:', req.headers);
     next();
 });
 
+// Connect to MongoDB
 connectToMongo();
-//----------------------available-routes---------------------//
+
+// Routes
 app.use('/api/auth', require('./routes/userRoute'));
 app.use('/api/notes', require('./routes/usernotes'));
-//----------------------available-routes---------------------//
 
+// Start the server
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
